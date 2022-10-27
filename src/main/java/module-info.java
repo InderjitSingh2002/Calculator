02d72d6218944288b65ce3e8836b9b5873c61c36
InderjitSingh2002 <200477140@student.georgianc.on.ca>
2022-10-27 10:15:14 -0400

    -created GUI -added stylesheet

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module portfolio.proj.calculator {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+
+
     opens portfolio.proj.calculator to javafx.fxml;
     exports portfolio.proj.calculator;
 }
