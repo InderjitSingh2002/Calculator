94029b106d46eb72805fee083978c9fe84e17380
InderjitSingh2002 <200477140@student.georgianc.on.ca>
2022-10-28 14:14:24 -0400

    -added the functionality to perform arithmetic operations on 2 integers at a time

--- src/main/java/portfolio/proj/calculator/CalculatorController.java
+++ src/main/java/portfolio/proj/calculator/CalculatorController.java
@@ -1,5 +1,6 @@
 package portfolio.proj.calculator;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -56,5 +57,82 @@ public class CalculatorController {
 
     @FXML
     private Button zeroButton;
+
+
+
+    String displayText = "";
+    int result;
+    String operator = "";
+    Calculator c = new Calculator(0,0);
+    @FXML
+    private void onClick(ActionEvent event) {
+        Button clickedButton = (Button) event.getSource();
+
+
+        switch(clickedButton.getText()) {
+            case "+" :
+                c.setNum1(c.getNum2());
+                c.setNum2(Integer.parseInt(displayText));
+                result = c.addFunc(c.getNum1(), c.getNum2());
+                displayText = "";
+                operator = "+";
+                break;
+            case "-" :
+                c.setNum1(c.getNum2());
+                c.setNum2(Integer.parseInt(displayText));
+                result = c.subtractFunc(c.getNum1(), c.getNum2());
+                displayText = "";
+                operator = "-";
+                break;
+            case "*" :
+                c.setNum1(c.getNum2());
+                c.setNum2(Integer.parseInt(displayText));
+                result = c.multiplyFunc(c.getNum1(), c.getNum2());
+                displayText = "";
+                operator = "*";
+                break;
+            case "/" :
+                c.setNum1(c.getNum2());
+                c.setNum2(Integer.parseInt(displayText));
+                result = c.divideFunc(c.getNum1(), c.getNum2());
+                displayText = "";
+                operator = "/";
+                break;
+            case "C" :
+                c.setNum1(0);
+                c.setNum2(0);
+                result = 0;
+                displayText = "";
+                break;
+            case "=":
+                switch (operator) {
+                    case "+" :
+                        c.setNum1(c.getNum2());
+                        c.setNum2(Integer.parseInt(displayText));
+                        result = c.addFunc(c.getNum1(), c.getNum2());
+                        break;
+                    case "-" :
+                        c.setNum1(c.getNum2());
+                        c.setNum2(Integer.parseInt(displayText));
+                        result = c.subtractFunc(c.getNum1(), c.getNum2());
+                        break;
+                    case "*" :
+                        c.setNum1(c.getNum2());
+                        c.setNum2(Integer.parseInt(displayText));
+                        result = c.multiplyFunc(c.getNum1(), c.getNum2());
+                        break;
+                    case "/" :
+                        c.setNum1(c.getNum2());
+                        c.setNum2(Integer.parseInt(displayText));
+                        result = c.divideFunc(c.getNum1(), c.getNum2());
+                        break;
+                }
+                displayText = String.valueOf(result);
+                break;
+            default :
+                displayText = displayText + clickedButton.getText();
+        }
+        resultTextField.setText(displayText);
+    }
 
 }
